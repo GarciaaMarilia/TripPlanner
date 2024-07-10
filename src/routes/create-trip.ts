bd310090f4bae2dcb21a7ee3e070324e618c3ecc
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2024-07-10 10:46:36 +0200

    create trip, participants and sending email

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,11 +1,16 @@
-import { FastifyInstance } from "fastify";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import dayjs from "dayjs";
 import z from "zod";
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br";
+import nodemailer from "nodemailer";
+import { FastifyInstance } from "fastify";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+
 import { prisma } from "../lib/prisma";
 import { getMailClient } from "../lib/mail";
-import nodemailer from "nodemailer";
 
+dayjs.locale('pt-br');
+dayjs.extend(localizedFormat);
 
 export async function createTrip(app: FastifyInstance) {
  app.withTypeProvider<ZodTypeProvider>().post(
@@ -18,11 +23,19 @@ export async function createTrip(app: FastifyInstance) {
      ends_at: z.coerce.date(),
      owner_name: z.string(),
      owner_email: z.string().email(),
+     emails_to_invite: z.array(z.string().email()),
     }),
    },
   },
   async (request) => {
-   const { destination, starts_at, ends_at } = request.body;
+   const {
+    destination,
+    starts_at,
+    ends_at,
+    owner_name,
+    owner_email,
+    emails_to_invite,
+   } = request.body; // a requisiçao post envia esses dados para a api
 
    if (dayjs(starts_at).isBefore(new Date())) {
     throw new Error("Invalid trip start date.");
@@ -37,21 +50,57 @@ export async function createTrip(app: FastifyInstance) {
      destination,
      starts_at,
      ends_at,
+     participants: {
+      createMany: {
+       data: [
+        {
+         name: owner_name,
+         email: owner_email,
+         is_owner: true,
+         is_confirmed: true,
+        },
+        ...emails_to_invite.map((email) => {
+         return { email };
+        }),
+       ],
+      },
+     },
     },
    });
+
+   const formattedStartDate = dayjs(starts_at).format("LL");
+   const formattedEndDate = dayjs(ends_at).format("LL");
+
+   const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm`;
 
    const mail = await getMailClient();
 
    const message = await mail.sendMail({
-    from:{
-        name: 'Marilia',
-        address:'garciaamarilia@gmail.com'
+    from: {
+     name: "Marilia",
+     address: "garciaamarilia@gmail.com",
     },
-    subject: 'Testando envio do e-mail',
-    html: `<p>Teste de envio de e-mail</p>`
-   })
+    to: {
+     name: owner_name,
+     address: owner_email,
+    },
+    subject: `Confirme sua viagem para ${destination}`,
+    html: `
+    <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+          <p>Você solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
+          <p></p>
+          <p>Para confirmar sua viagem, clique no link abaixo:</p>
+          <p></p>
+          <p>
+            <a href="${confirmationLink}">Confirmar viagem</a>
+          </p>
+          <p></p>
+          <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
+        </div>
+    `.trim(),
+   });
 
-   console.log(nodemailer.getTestMessageUrl(message))
+   console.log(nodemailer.getTestMessageUrl(message));
 
    return {
     tripId: trip.id,
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,26 @@
 import fastify from "fastify";
-import { createTrip } from "./routes/create-trip";
+import cors from "@fastify/cors";
+
 import {
  serializerCompiler,
  validatorCompiler,
 } from "fastify-type-provider-zod";
 
+import { createTrip } from "./routes/create-trip";
+import { confirmTrip } from "./routes/confirm-trip";
+
 const app = fastify();
+
+app.register(cors, {
+ // garantir a segurança e dizer qual frontend pode acessar o backend. Por enquanto, estamos em produçao, entao, vamos setar como true e todo frontend podera acessar, porém, em produçao, mudaermos isso
+ origin: "*",
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
+app.register(confirmTrip);
 
 app.listen({ port: 3333 }).then(() => {
  console.log("Server running");
