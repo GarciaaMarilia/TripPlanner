851875656d49293199e8a16e241cf1477b7e421e
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2024-10-25 17:02:53 +0200

    the host

--- src/server.ts
+++ src/server.ts
@@ -54,6 +54,6 @@ app.register(deleteActivity);
 app.register(getParticipants);
 app.register(confirmParticipants);
 
-app.listen({ port: env.PORT }).then((port) => {
+app.listen(env.PORT, env.THE_HOST).then((port) => {
  console.log("Server running", port);
 });
